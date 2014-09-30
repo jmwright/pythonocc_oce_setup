ec6d3e92c436719f76ca54888b7e4700bec222c8
Jeremy Wright <jwright@innovationsts.com>
2014-09-30 09:55:27 -0400

    Split the git tag out into a variable.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #This script needs to be run with superuser priveleges
 
+#Set up our variables
+TAG="OCE-0.12"
+
 # apt-get update
 # apt-get upgrade
 
@@ -8,7 +11,7 @@
 # apt-get install -y git
 
 # #Clone OCE and try to compile it
-# git clone --branch OCE-0.12 git://github.com/tpaviot/oce.git
+# git clone --branch $TAG git://github.com/tpaviot/oce.git
 
 #Move into the oce directory and prepare to compile it
 cd oce; mkdir build; cd build
