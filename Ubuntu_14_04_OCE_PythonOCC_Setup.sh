7d1e8f688bb35a077f0dfd09df942a6005f9a47c
Jeremy Wright <jwright@innovationsts.com>
2014-09-30 12:50:46 -0400

    Added make and make install steps.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -16,3 +16,6 @@ git clone --branch $TAG git://github.com/tpaviot/oce.git
 #Move into the oce directory and prepare to run cmake
 cd oce; mkdir build; cd build
 cmake .. -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
+
+#Build and install
+make && make install/strip
