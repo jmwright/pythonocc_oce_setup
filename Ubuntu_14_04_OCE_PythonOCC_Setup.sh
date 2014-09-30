ac7738f4b29ad260663805f5a75db88c38af2836
Jeremy Wright <jwright@innovationsts.com>
2014-09-30 09:51:48 -0400

    Shouldn't have been trying to cd to mkdir.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -11,4 +11,4 @@
 # git clone --branch OCE-0.12 git://github.com/tpaviot/oce.git
 
 #Move into the oce directory and prepare to compile it
-cd oce; mkdir build; cd mkdir
+cd oce; mkdir build; cd build
