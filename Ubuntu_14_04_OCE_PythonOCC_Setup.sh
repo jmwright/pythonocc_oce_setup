30c550126b0bd78625b880d207788e08d1e496a5
Jeremy Wright <jwright@innovationsts.com>
2014-09-30 09:37:24 -0400

    Added creating the build dir.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 #This script needs to be run with superuser priveleges
 
-apt-get update
-apt-get upgrade
+# apt-get update
+# apt-get upgrade
 
-#Install the pre-requisites
-apt-get install -y git
+# #Install the pre-requisites
+# apt-get install -y git
 
-#Clone OCE and try to compile it
-git clone --branch OCE-0.12 git://github.com/tpaviot/oce.git
+# #Clone OCE and try to compile it
+# git clone --branch OCE-0.12 git://github.com/tpaviot/oce.git
+
+#Move into the oce directory and prepare to compile it
+cd oce; mkdir build; cd mkdir
