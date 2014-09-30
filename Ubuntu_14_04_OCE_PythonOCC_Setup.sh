3b7045737d3b7c20a2003917f184ea4d93197112
Jeremy Wright <jwright@innovationsts.com>
2014-09-30 10:07:35 -0400

    Added cmake.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -4,14 +4,15 @@
 #Set up our variables
 TAG="OCE-0.12"
 
-# apt-get update
-# apt-get upgrade
+apt-get update
+apt-get upgrade
 
-# #Install the pre-requisites
-# apt-get install -y git
+#Install the pre-requisites
+apt-get install -y git cmake
 
-# #Clone OCE and try to compile it
-# git clone --branch $TAG git://github.com/tpaviot/oce.git
+#Clone OCE and try to compile it
+git clone --branch $TAG git://github.com/tpaviot/oce.git
 
 #Move into the oce directory and prepare to compile it
 cd oce; mkdir build; cd build
+cmake ..
