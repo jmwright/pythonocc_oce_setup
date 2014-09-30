ed7abc892e25012bda00d770b2ddea9ff5070fc3
Jeremy Wright <jwright@innovationsts.com>
2014-09-30 11:12:18 -0400

    Added g++ compiler switch for cmake and gcc/g++ instals.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -8,11 +8,11 @@ apt-get update
 apt-get upgrade
 
 #Install the pre-requisites
-apt-get install -y git cmake libx11-dev mesa-common-dev freeglut3-dev libfreetype6-dev libc6-dev libstdc++ libgcc build-essential
+apt-get install -y git cmake libx11-dev mesa-common-dev freeglut3-dev libfreetype6-dev libc6-dev libstdc++ libgcc build-essential gcc g++
 
 #Clone OCE and try to compile it
 git clone --branch $TAG git://github.com/tpaviot/oce.git
 
 #Move into the oce directory and prepare to run cmake
 cd oce; mkdir build; cd build
-cmake .. -DCMAKE_C_COMPILER=/usr/bin/gcc
+cmake .. -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
