dfaa1c8daf72164c5b5c8093ae837799286ccb47
Jeremy Wright <jwright@innovationsts.com>
2014-10-07 16:43:15 -0400

    Started completely reworking the script based on a Travis configuration I found for PythonOCC today.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -3,34 +3,29 @@
 
 #Set up our variables
 #TAG="OCE-0.12"
-OCETAG="OCE-0.16"
+#OCETAG="OCE-0.16"
 PYTHONOCCTAG="0.16-pre-1"
 POCCMAKEPATH="pythonocc-core/cmake-build"
 
-# apt-get update
-# apt-get upgrade
-
-# #Install the pre-requisites
-# apt-get install -fy git cmake libx11-dev mesa-common-dev freeglut3-dev libfreetype6-dev libc6-dev libstdc++6 libstdc++-4.8-dev build-essential gcc g++ libftgl-dev xorg-dev tcl-dev tk-dev
-
-# #Clone OCE and try to compile it
-# git clone --branch $OCETAG git://github.com/tpaviot/oce.git
-
-# #Move into the oce directory and prepare to run cmake
-# cd oce; mkdir build; cd build
-# cmake .. -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DOCE_DRAW=ON
-
-# #Build and install
-# make && make install/strip
+sudo apt-get install swig
+sudo add-apt-repository "deb http://ppa.launchpad.net/freecad-maintainers/oce-release/ubuntu precise main" -y
+sudo apt-get update -q
+sudo apt-get install liboce-ocaf-dev oce-draw
+sudo apt-get install python-wxgtk2.8
+sudo python -c "import wx"
+sudo apt-get install python-qt4 python-qt4-gl
+sudo python -c "from PyQt4 import QtGui, QtCore, QtOpenGL"
+sudo apt-get install python-pyside
+sudo python -c "from PySide import QtGui, QtCore, QtOpenGL"
 
 #Install PythonOCC requirements
 apt-get install swig python-sympy
 
 #Clone PythonOCC so we can start building the components we need
-git clone https://github.com/tpaviot/pythonocc-core.git
+git clone --branch $PYTHONOCCTAG https://github.com/tpaviot/pythonocc-core.git
 
 mkdir $POCCMAKEPATH && cd $POCCMAKEPATH
 
 #Guild the geom extension
-cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -D OCC_INCLUDE_PATH="/usr/local/include/oce" ..
+cmake -DOCE_INCLUDE_PATH="/usr/local/include/oce" -DOCE_LIB_PATH="/usr/local/lib/oce-0.16/" ..
 make && make install
