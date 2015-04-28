97420b4bb786f5022a6e092caf104f96ebe5e383
Jeremy Wright <wrightjmf@gmail.com>
2015-04-28 08:13:25 -0400

    Made it so that the OCE and PythonOCC can be sync'd because they will break during compilation otherwise.

--- Ubuntu_14_04_OCE_PythonOCC_Setup.sh
+++ Ubuntu_14_04_OCE_PythonOCC_Setup.sh
@@ -2,30 +2,29 @@
 #This script needs to be run with superuser priveleges
 
 #Set up our variables
-#TAG="OCE-0.12"
-#OCETAG="OCE-0.16"
+OCETAG="OCE-0.16" #This and the PythonOCC version must match
 PYTHONOCCTAG="0.16.0"
+OCEMAKEPATH="oce/build"
 POCCMAKEPATH="pythonocc-core/cmake-build"
 
 #Attempt to install all the pre-requisites
-apt-get install -fy git cmake libx11-dev mesa-common-dev freeglut3-dev libfreetype6-dev libc6-dev libstdc++6 libstdc++-4.8-dev build-essential gcc g++ libftgl-dev xorg-dev tcl-dev tk-dev python-dev
+apt-get install -fy git cmake libx11-dev mesa-common-dev freeglut3-dev libfreetype6-dev libc6-dev libstdc++6 libstdc++-4.8-dev build-essential gcc g++ libftgl-dev xorg-dev tcl-dev tk-dev python-dev swig python-wxgtk2.8 python-qt4 python-qt4-gl python-pyside swig python-sympy
 
-apt-get install -y swig
-add-apt-repository "deb http://ppa.launchpad.net/freecad-maintainers/oce-release/ubuntu precise main" -y
-apt-get update -q
-apt-get install -y --force-yes liboce-ocaf-dev oce-draw liboce-modeling-dev liboce-foundation-dev
-apt-get install -y python-wxgtk2.8
+git clone --branch $OCETAG https://github.com/tpaviot/oce.git
+
+mkdir $OCEMAKEPATH && cd $OCEMAKEPATH
+
+cmake ..
+make
+make install/strip
+
 python -c "import wx"
-apt-get install -y python-qt4 python-qt4-gl
 python -c "from PyQt4 import QtGui, QtCore, QtOpenGL"
-apt-get install -y python-pyside
 python -c "from PySide import QtGui, QtCore, QtOpenGL"
 
-#Install PythonOCC requirements
-apt-get install -y swig python-sympy
+cd ../../
 
 #Clone PythonOCC so we can start building the components we need
-#git clone https://github.com/tpaviot/pythonocc-core.git
 git clone --branch $PYTHONOCCTAG https://github.com/tpaviot/pythonocc-core.git
 
 mkdir $POCCMAKEPATH && cd $POCCMAKEPATH
